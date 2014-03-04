a170e60c447297108153241f731582ef5b06c8a0
Todd Hayton <thayton@neekanee.com>
2014-03-04 09:16:24 -0500

    norcross

---
+++ plugins/ga/bidsaspx/norcross.py
@@ -0,0 +1,19 @@
+from bidmap.bidscrapers.bidsaspx.bidsaspx import BidsAspxBidScraper
+
+GOVINFO = {
+    'name': 'City of Norcross Georgia',
+    'location': 'Norcross, GA',
+
+    'home_page_url': 'http://ga-norcross.civicplus.com/bids.aspx',
+    'bids_page_url': 'http://ga-norcross.civicplus.com'
+}
+
+def get_scraper():
+    return BidsAspxBidScraper(GOVINFO)
+
+if __name__ == '__main__':
+    bid_scraper = get_scraper()
+    bid_scraper.scrape_bids()
+
+
+
