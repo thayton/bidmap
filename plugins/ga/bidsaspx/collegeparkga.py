9d1a7dff135c097f5fa8582351e9ac57eb8bbaee
Todd Hayton <thayton@neekanee.com>
2014-03-04 08:44:56 -0500

    collegeparkga

---
+++ plugins/ga/bidsaspx/collegeparkga.py
@@ -0,0 +1,19 @@
+from bidmap.bidscrapers.bidsaspx.bidsaspx import BidsAspxBidScraper
+
+GOVINFO = {
+    'name': 'College Park Georgia',
+    'location': 'College Park, GA',
+
+    'home_page_url': 'http://www.collegeparkga.com',
+    'bids_page_url': 'http://www.collegeparkga.com/bids.aspx'
+}
+
+def get_scraper():
+    return BidsAspxBidScraper(GOVINFO)
+
+if __name__ == '__main__':
+    bid_scraper = get_scraper()
+    bid_scraper.scrape_bids()
+
+
+
