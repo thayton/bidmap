3baeb37488ce5a093d6699a2160366c04968b335
Todd Hayton <thayton@neekanee.com>
2014-03-04 09:51:56 -0500

    alpharetta

---
+++ plugins/ga/link/alpharetta.py
@@ -0,0 +1,52 @@
+import re, urlparse
+
+from bid import Bid
+from bidmap.bidscrapers.bidscraper import BidScraper
+from bidmap.htmlparse.soupify import soupify
+
+GOVINFO = {
+    'name': 'Alpharetta Geogia',
+    'location': 'Alpharetta, GA',
+
+    'home_page_url': 'http://www.alpharetta.ga.us',
+    'bids_page_url': 'http://www.alpharetta.ga.us/index.php?m=procurement'
+}
+
+class AlpharettaGaBidScraper(BidScraper):
+    def __init__(self):
+        super(AlpharettaGaBidScraper, self).__init__(GOVINFO)
+
+    def scrape_bid_links(self, url):
+        bids = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        r = re.compile(r'^index\.php\?m=procurement&id=\d+$')
+
+        for a in s.findAll('a', href=r):
+            tr = a.findParent('tr')
+            td = tr.findAll('td')
+
+            # Verify open status
+            if td[-1].text != 'OPEN':
+                continue
+
+            bid = Bid()
+            bid.title = a.text
+            bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            bids.append(bid)
+
+        return bids
+
+    def scrape_bids(self):
+        bid_list = self.scrape_bid_links(self.gov['bids_page_url'])
+        for bid in bid_list:
+            print bid
+
+def get_scraper():
+    return AlpharettaGaBidScraper()
+
+if __name__ == '__main__':
+    bid_scraper = get_scraper()
+    bid_scraper.scrape_bids()
