5308eb21bee3c4f33a481ae1427bafe405a8be8a
Todd Hayton <thayton@neekanee.com>
2014-03-12 11:20:34 -0400

    cityofnewnan

---
+++ plugins/ga/link/cityofnewnan.py
@@ -0,0 +1,40 @@
+import re, urlparse
+
+from bid import Bid
+from bidmap.bidscrapers.bidscraper import BidScraper
+from bidmap.htmlparse.soupify import soupify
+
+GOVINFO = {
+    'name': 'Newnan Geogia',
+    'location': 'Newnan, GA',
+
+    'home_page_url': 'http://www.cityofnewnan.org',
+    'bids_page_url': 'http://www.cityofnewnan.org/current_open_bids.php'
+}
+
+class NewnanGaBidScraper(BidScraper):
+    def __init__(self):
+        super(NewnanGaBidScraper, self).__init__(GOVINFO)
+
+    def scrape_bid_links(self, url):
+        bids = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        ul = s.find('ul', id='toplevel')
+
+        for a in ul.findAll('a'):
+            bid = Bid()
+            bid.title = a.text
+            bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            bids.append(bid)
+
+        return bids
+
+def get_scraper():
+    return NewnanGaBidScraper()
+
+if __name__ == '__main__':
+    bid_scraper = get_scraper()
+    bid_scraper.scrape_bids()
