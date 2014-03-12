ecebc1874af0b30547197a851e6bd2e2ee542910
Todd Hayton <thayton@neekanee.com>
2014-03-12 11:06:09 -0400

    romefloyd

---
+++ plugins/ga/link/romefloyd.py
@@ -0,0 +1,40 @@
+import re, urlparse
+
+from bid import Bid
+from bidmap.bidscrapers.bidscraper import BidScraper
+from bidmap.htmlparse.soupify import soupify
+
+GOVINFO = {
+    'name': 'Rome Geogia',
+    'location': 'Rome, GA',
+
+    'home_page_url': 'http://www.romefloyd.com',
+    'bids_page_url': 'http://www.romefloyd.com/Administration/BidsandRFPs/RomeCity/tabid/491/agentType/ViewSearch/CustomFieldIDs/9/SearchValues/Open/sortBy/Type/sortDir/Ascending/Default.aspx'
+}
+
+class RomeGaBidScraper(BidScraper):
+    def __init__(self):
+        super(RomeGaBidScraper, self).__init__(GOVINFO)
+
+    def scrape_bid_links(self, url):
+        bids = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        r = re.compile(r'/View/PropertyID/\d+/')
+
+        for a in s.findAll('a', href=r):
+            bid = Bid()
+            bid.title = a.text
+            bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            bids.append(bid)
+
+        return bids
+
+def get_scraper():
+    return RomeGaBidScraper()
+
+if __name__ == '__main__':
+    bid_scraper = get_scraper()
+    bid_scraper.scrape_bids()
