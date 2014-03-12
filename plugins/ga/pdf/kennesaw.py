bb6cfa528bc2368837da2a8ed39447f8da37d598
Todd Hayton <thayton@neekanee.com>
2014-03-12 11:24:01 -0400

    kennesaw

---
+++ plugins/ga/pdf/kennesaw.py
@@ -0,0 +1,40 @@
+import re, urlparse
+
+from bid import Bid
+from bidmap.bidscrapers.bidscraper import BidScraper
+from bidmap.htmlparse.soupify import soupify
+
+GOVINFO = {
+    'name': 'Kennesaw Geogia',
+    'location': 'Kennesaw, GA',
+
+    'home_page_url': 'http://www.kennesaw-ga.gov',
+    'bids_page_url': 'http://www.kennesaw-ga.gov/business/doing-business/bids-a-rfps'
+}
+
+class KennesawGaBidScraper(BidScraper):
+    def __init__(self):
+        super(KennesawGaBidScraper, self).__init__(GOVINFO)
+
+    def scrape_bid_links(self, url):
+        bids = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        r = re.compile(r'\.pdf$')
+
+        for a in s.findAll('a', href=r):
+            bid = Bid()
+            bid.title = a.text
+            bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            bids.append(bid)
+
+        return bids
+
+def get_scraper():
+    return KennesawGaBidScraper()
+
+if __name__ == '__main__':
+    bid_scraper = get_scraper()
+    bid_scraper.scrape_bids()
