174c3e9f03608731e4687a7025336464f8a99f26
Todd Hayton <thayton@neekanee.com>
2014-03-12 13:36:27 -0400

    cityofwinder

---
+++ plugins/ga/link/cityofwinder.py
@@ -0,0 +1,42 @@
+import re, urlparse
+
+from bid import Bid
+from bidmap.bidscrapers.bidscraper import BidScraper
+from bidmap.htmlparse.soupify import soupify
+
+GOVINFO = {
+    'name': 'Winder Geogia',
+    'location': 'Winder, GA',
+
+    'home_page_url': 'http://www.cityofwinder.com',
+    'bids_page_url': 'http://www.cityofwinder.com/index.aspx?page=245'
+}
+
+class WinderGaBidScraper(BidScraper):
+    def __init__(self):
+        super(WinderGaBidScraper, self).__init__(GOVINFO)
+
+    def scrape_bid_links(self, url):
+        bids = []
+
+        self.br.open(url)
+
+        s = soupify(self.br.response().read())
+        r = re.compile(r'^_ctl\d+_listDataGrid$')
+        t = s.find('table', id=r)
+        r = re.compile(r'^index\.aspx\?recordid=\d+')
+
+        for a in t.findAll('a', href=r):
+            bid = Bid()
+            bid.title = a.text
+            bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            bids.append(bid)
+
+        return bids
+
+def get_scraper():
+    return WinderGaBidScraper()
+
+if __name__ == '__main__':
+    bid_scraper = get_scraper()
+    bid_scraper.scrape_bids()
