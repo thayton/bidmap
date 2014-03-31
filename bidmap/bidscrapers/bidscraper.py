587d9028b06d9ed2af2f69d59979ee1b7459b113
Todd Hayton <thayton@neekanee.com>
2014-03-31 13:59:02 -0400

    delta for new bids to add / old bids to remove

--- bidmap/bidscrapers/bidscraper.py
+++ bidmap/bidscrapers/bidscraper.py
@@ -81,15 +81,81 @@ class BidScraper(object):
         scraping routine specific to each site '''
         pass
 
+    def get_bid_cmp(self):
+        """
+        Bid comparison function to be used when comparing whether two bids are
+        equivalent. The default is to assume that if the url/data fields of two
+        bids objects are equivalent then the two bids are the same. 
+
+        Inherited classes may override this field for more complex comparison 
+        operations.
+        """
+        return lambda x,y: x.url == y.url and x.url_data == y.url_data
+
     def prune_unlisted_bids(self, bid_list):
-        ''' Remvoe bids from the database that are no longer 
-        listed on the site '''
-        pass
+        """
+        Remove dead bids from the database - these are bids whose links 
+        are in the  database that are no longer listed on the government
+        bids site.
+        
+        Callers can set use_bid_cmp=True and then define get_bid_cmp()
+        to return a comparison function of their choosing if the url,url_data
+        comparison used here does not suffice.
+        """
+        num_deleted = 0
+
+        if use_bid_cmp:
+            cmp = self.get_bid_cmp()
+
+            for bid in self.org.bid_set.all():
+                if not find(cmp, bid, listed_bids):
+                    self.logger.debug('Deleting bid (%s) for org %s' % (bid.url, self.org))
+                    bid.delete()
+                    num_deleted += 1
+        else:
+            stored_bids_set = set([('%s' % j.url, '%s' % j.url_data) for j in self.org.bid_set.all()])
+            listed_bids_set = set([('%s' % j.url, '%s' % j.url_data) for j in listed_bids])
+
+            bids_to_delete = stored_bids_set - listed_bids_set
+
+            self.logger.info('%d bids to delete' % len(bids_to_delete))
+
+            for entry in bids_to_delete:
+                for bid in self.org.bid_set.filter(url=entry[0], url_data=entry[1]):
+                    self.logger.debug('Deleting bid (%s) for org %s' % (bid.url, self.org))
+                    bid.delete()
+                    num_deleted += 1
+
+        self.logger.info('Deleted %d unlisted bids' % num_deleted)
 
     def new_bids(self, bid_list):
-        ''' Determine which bids on site are new when 
-        compared to the database of bids already downloaded'''
-        return bid_list
+        ''' 
+        Determine which bids on site are new when compared to the database of bids 
+        already downloaded
+
+        Callers can set use_job_cmp=True and then define get_job_cmp() to return a 
+        comparison function of their choosing if the url,url_data comparison used 
+        here does not suffice.
+        '''
+        self.logger.debug('Extracting new bids from %d bids listed' % len(listed_bids))
+        new_bids = []
+
+        if use_bid_cmp:
+            cmp = self.get_bid_cmp()
+
+            for bid in listed_bids:
+                if not find(cmp, bid, self.org.bid_set.all()) and \
+                   not find(cmp, bid, new_bids): # don't allow dups in new_bids
+                    self.logger.debug('New bid (%s) at org %s' % (bid.url, self.org))
+                    new_bids.append(bid)
+        else:
+            for bid in listed_bids:
+                if self.org.bid_set.filter(url=bid.url, url_data=bid.url_data).count() == 0:
+                    self.logger.debug('New bid (%s) at org %s' % (bid.url, self.org))
+                    new_bids.append(bid)
+
+        self.logger.info('%d new bid listings' % len(new_bids))            
+        return new_bids
 
     def scrape_bid_description(self, bid):
         '''
@@ -107,6 +173,3 @@ class BidScraper(object):
         new_bids = self.new_bids(bid_list)
         for bid in new_bids:
             self.scrape_bid_description(bid)
-
-
-
