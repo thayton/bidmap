0d3493f6b17555e49a484d07709ac02cf632702a
Todd Hayton <thayton@neekanee.com>
2014-03-31 11:49:42 -0400

    code to extract text from HTML

--- bidmap/htmlparse/soupify.py
+++ bidmap/htmlparse/soupify.py
@@ -1,4 +1,7 @@
+import re, htmlentitydefs
+
 from bs4 import BeautifulSoup, Comment, Tag
+from django.utils.encoding import smart_str, smart_unicode
 
 def soupify(page):
     s = BeautifulSoup(page, 'lxml')
@@ -15,3 +18,38 @@ def soupify(page):
         c.extract()
 
     return s
+
+##
+# Removes HTML or XML character references and entities from a text string.
+#
+# @param text The HTML (or XML) source text.
+# @return The plain text, as a Unicode string, if necessary.
+
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
+
+def get_all_text(s):
+    t = s.findAll(text=True)
+    t = unescape(' '.join(t))
+    t = smart_str(t)	
+    t = t.decode('utf8') 
+    t = ' '.join(t.split())
+    return t
