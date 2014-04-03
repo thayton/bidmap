fd9f124062b46f6c9d1b44c9f3192405f5047256
Todd Hayton <thayton@neekanee.com>
2014-04-03 16:10:36 -0400

    mariettaga

--- plugins/ga/link/mariettaga.py
+++ plugins/ga/link/mariettaga.py
@@ -1,7 +1,7 @@
-import re, urlparse
+import re, urlparse, datetime
 
 from bidmap.bidscrapers.bidscraper import BidScraper
-from bidmap.htmlparse.soupify import soupify
+from bidmap.htmlparse.soupify import soupify, get_all_text
 
 from bidmapdb.models import *
 
@@ -24,23 +24,37 @@ class MariettaGaBidScraper(BidScraper):
 
         s = soupify(self.br.response().read())
         r = re.compile(r'viewbid\?id=\d+$')
+        v = re.compile(r'(\d{1,2})/(\d{1,2})/(\d{4})')
 
         for a in s.findAll('a', href=r):
             tr = a.findParent('tr')
             td = tr.findAll('td')
 
+            z = re.search(v, td[-1].text)
+            if z:
+                m,d,y = z.groups()
+
             bid = Bid(org=self.org)
             bid.title = td[1].span.text
             bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
+
+            if z:
+                bid.due_date = datetime.date(day=int(d), month=int(m), year=int(y))
+
             bid.location = self.org.location
             bids.append(bid)
 
         return bids
 
-    def scrape_bids(self):
-        bid_list = self.scrape_bid_links(self.gov['bids_page_url'])
-        for bid in bid_list:
-            print bid
+    def scrape_bid_description(self, bid):
+        self.br.open(bid.url)
+
+        s = soupify(self.br.response().read())
+        r = re.compile(r'PurchasingBids')
+        t = s.find('table', id=r)
+
+        bid.description = get_all_text(t)
+        bid.save()
 
 def get_scraper():
     return MariettaGaBidScraper()
