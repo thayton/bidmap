a096af7e5be9f29603c668a40f0f7923b5faf9a5
Todd Hayton <thayton@neekanee.com>
2014-04-03 15:25:09 -0400

    cityofcovington

--- plugins/ga/link/cityofcovington.py
+++ plugins/ga/link/cityofcovington.py
@@ -1,7 +1,7 @@
-import re, urlparse
+import re, urlparse, datetime
 
 from bidmap.bidscrapers.bidscraper import BidScraper
-from bidmap.htmlparse.soupify import soupify
+from bidmap.htmlparse.soupify import soupify, get_all_text
 
 from bidmapdb.models import *
 
@@ -26,15 +26,37 @@ class CovingtonGaBidScraper(BidScraper):
         x = {'summary': 'Current Bid Postings '}
         t = s.find('table', attrs=x)
         r = re.compile(r'/Bids/')
+        v = re.compile(r'(\d{1,2})/(\d{1,2})/(\d{4})')
 
         for a in t.findAll('a', href=r):
+            tr = a.findParent('tr')
+            td = tr.findAll('td')
+
+            z = re.search(v, td[-2].text)
+            if z:
+                m,d,y = z.groups()
+
             bid = Bid(org=self.org)
             bid.title = a.text
             bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
+
+            if z:
+                bid.due_date = datetime.date(day=int(d), month=int(m), year=int(y))
+
             bid.location = self.org.location
             bids.append(bid)
 
         return bids
+
+    def scrape_bid_description(self, bid):
+        self.br.open(bid.url)
+
+        s = soupify(self.br.response().read())
+        x = {'class': 'ms-formtable'}
+        t = s.find('table', attrs=x)
+
+        bid.description = get_all_text(t)
+        bid.save()
 
 def get_scraper():
     return CovingtonGaBidScraper()
