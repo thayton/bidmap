47005b63152ff14d697ac1357b3c4700659dc0ff
Todd Hayton <thayton@neekanee.com>
2014-06-25 10:41:57 -0400

    woburnma

---
+++ plugins/ma/bidsaspx/woburnma.py
@@ -0,0 +1,19 @@
+from bidmap.bidscrapers.bidsaspx.bidsaspx import BidsAspxBidScraper
+
+GOVINFO = {
+    'name': 'Woburn Massachusetts',
+    'location': 'Woburn, MA',
+
+    'home_page_url': 'http://www.woburnma.gov',
+    'bids_page_url': 'http://www.woburnma.gov/Bids.aspx'
+}
+
+def get_scraper():
+    return BidsAspxBidScraper(GOVINFO)
+
+if __name__ == '__main__':
+    bid_scraper = get_scraper()
+    bid_scraper.scrape_bids()
+
+
+
