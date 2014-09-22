8df26f74f23a34a038bf8debfce48329d5bb7a42
Todd Hayton <thayton@neekanee.com>
2014-09-22 11:07:35 -0400

    gaithersburgmd

--- plugins/md/link/gaithersburgmd.py
+++ plugins/md/link/gaithersburgmd.py
@@ -2,14 +2,14 @@ import re, urlparse
 
 from bid import Bid
 from bidmap.bidscrapers.bidscraper import BidScraper
-from bidmap.htmlparse.soupify import soupify
+from bidmap.htmlparse.soupify import soupify, get_all_text
 
 GOVINFO = {
     'name': 'Gaithersburg Maryland',
     'location': 'Gaithersburg, MD',
 
     'home_page_url': 'http://www.gaithersburgmd.gov',
-    'bids_page_url': 'http://www.gaithersburgmd.gov/poi/default.asp?POI_ID=210'
+    'bids_page_url': 'http://www.gaithersburgmd.gov/government/procurement/current-bids'
 }
 
 class GaithersburgBidScraper(BidScraper):
@@ -22,17 +22,37 @@ class GaithersburgBidScraper(BidScraper):
         self.br.open(url)
 
         s = soupify(self.br.response().read())
-        r = re.compile(r'^/poi/default\.asp\?POI_ID=\d+&TOC=\d+')
-        x = {'type': 'disc'}
-        u = s.find('ul', attrs=x)
+        r = re.compile(r'/current-bids/[^/]+$')
 
-        for a in u.findAll('a', href=r):
+        for a in s.findAll('a', href=r):
+            if not a.h1:
+                continue
+
             bid = Bid()
             bid.title = a.text
             bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
             bids.append(bid)
 
         return bids
+
+    def scrape_bid_description(self, bid):
+        self.br.open(bid.url)
+        
+        s = soupify(self.br.response().read())
+        x = {'class': 'body-content'}
+        y = {'class': 'promo'}
+        n = s.find('section', attrs=y)
+
+        bid.contact = n.text
+
+        m = re.search(self.email_regex, n.text)
+        if m:
+            bid.email = m.group(0)
+
+        d = s.find('div', attrs=x)
+
+        bid.desc = get_all_text(d)
+        bid.save()
 
 def get_scraper():
     return GaithersburgBidScraper()
