44982b6edcb54d9c290f9e059d644fb52e408e87
Todd Hayton <thayton@neekanee.com>
2014-09-22 11:58:24 -0400

    fix description and contact

--- plugins/md/link/harfordcountymd.py
+++ plugins/md/link/harfordcountymd.py
@@ -1,8 +1,9 @@
 import re, urlparse
 
-from bid import Bid
 from bidmap.bidscrapers.bidscraper import BidScraper
-from bidmap.htmlparse.soupify import soupify
+from bidmap.htmlparse.soupify import soupify, get_all_text
+
+from bidmapdb.models import *
 
 GOVINFO = {
     'name': 'Harford County Maryland',
@@ -25,12 +26,30 @@ class HarfordCountyBidScraper(BidScraper):
         r = re.compile(r'^BidBoard\.cfm\?BidID=\d+$')
 
         for a in s.findAll('a', href=r):
-            bid = Bid()
+            bid = Bid(org=self.org)
             bid.title = a.text
             bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
+            bid.location = self.org.location
             bids.append(bid)
 
         return bids
+
+    def scrape_bid_description(self, bid):
+        self.br.open(bid.url)
+        
+        s = soupify(self.br.response().read())
+        r = re.compile(r'^Agent')
+        f = lambda x: x.name == 'b' and re.search(r, x.text)
+        b = s.find(f)
+
+        if b:
+            t = b.findParent('table')
+            bid.contact = get_all_text(t)
+
+        b = s.find('blockquote')
+
+        bid.description = get_all_text(b)
+        bid.save()
 
 def get_scraper():
     return HarfordCountyBidScraper()
