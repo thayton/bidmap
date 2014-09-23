41aaa0332a7a03cff62736296a7bb6e55f84c9fa
Todd Hayton <thayton@neekanee.com>
2014-09-22 20:08:11 -0400

    extract closing date

--- plugins/md/link/pgebid.py
+++ plugins/md/link/pgebid.py
@@ -1,8 +1,9 @@
-import re, urlparse
+import re, urlparse, datetime
 
-from bid import Bid
 from bidmap.bidscrapers.bidscraper import BidScraper
-from bidmap.htmlparse.soupify import soupify
+from bidmap.htmlparse.soupify import soupify, get_all_text
+
+from bidmapdb.models import *
 
 GOVINFO = {
     'name': 'Prince Georges County Maryland',
@@ -28,12 +29,31 @@ class PrinceGeorgesCountyBidScraper(BidScraper):
             tr = a.findParent('tr')
             td = tr.findAll('td')
 
-            bid = Bid()
+            z = re.search(self.date_regex, td[-2].text)
+            if z:
+                m,d,y = z.groups()
+
+            bid = Bid(org=self.org)
             bid.title = td[1].text
             bid.url = urlparse.urljoin(self.br.geturl(), a['href'])
+
+            if z:
+                bid.due_date = datetime.date(day=int(d), month=int(m), year=int(y))
+
+            bid.location = self.org.location
             bids.append(bid)
 
         return bids
+
+    def scrape_bid_description(self, bid):
+        self.br.open(bid.url)
+
+        s = soupify(self.br.response().read())
+        h = s.find('h1')
+        t = h.findParent('table')
+
+        bid.description = get_all_text(t)
+        bid.save()
 
 def get_scraper():
     return PrinceGeorgesCountyBidScraper()
